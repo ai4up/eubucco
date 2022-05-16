50896f2baf9948f6e66dfade6b390f08286520b6
Florian Nachtigall <nachtigall.florian@gmail.com>
2022-05-16 13:54:15 +0200

    Add feature engineering step

---
+++ database/feature-engineering/fts.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+from ft_eng.ft_eng import create_features
+from ufo_map.Utils.helpers import arg_parser
+
+PROJECT_SRC_PATH = os.path.realpath(os.path.join(__file__, '..', '..', '..', 'eubucco'))
+sys.path.append(PROJECT_SRC_PATH)
+
+# utilize Slurm's concurrent job scheduling by mapping SLURM_ARRAY_TASK_ID to city indices
+city_idx = arg_parser(['i']).i
+print(city_idx)
+
+create_features('netherlands',
+                city_idx=city_idx,
+                bld=True,
+                blk=True,
+                bld_d=True,
+                blk_d=True,
+                int_=True,
+                str_=True,
+                sbb_=True,
+                city_level=True,
+                left_over=False,
+                ua_mode=False,
+                data_dir='/p/projects/eubucco/data/2-database-city-level')
