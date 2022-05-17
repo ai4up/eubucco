e773fc1aaf439f0ee15ef357c59ef7c75814d839
Florian Nachtigall <nachtigall.florian@gmail.com>
2022-05-17 12:48:05 +0200

    Bump ufo-map submodule and incorporate changes
    * incorporate submodule get_stats() -> write_stats() changes
    * format streets.py, wsf.py & ft_eng.py according to PEP 8
    * trim trailing whitespaces
    * rename CRS constant to CRS_UNI for consistency across modules
    * write feature engineering stats as last step of create_features()
    * align stats directory of parse_streets() with other functions

--- eubucco/ft_eng/ft_eng.py
+++ eubucco/ft_eng/ft_eng.py
@@ -4,13 +4,13 @@ import time
 import pandas as pd
 import numpy as np
 
-from ufo_map.Utils.helpers import import_csv_w_wkt_to_gdf, get_all_paths
+from ufo_map.Utils.helpers import import_csv_w_wkt_to_gdf, get_all_paths, write_stats
 from ufo_map.Feature_engineering.buildings import features_building_level, features_buildings_distance_based
 from ufo_map.Feature_engineering.blocks import features_block_level, features_blocks_distance_based
 from ufo_map.Feature_engineering.streets import *
 from ufo_map.Feature_engineering.city_level import *
 
-CRS = 3035
+CRS_UNI = 'EPSG:3035'
 
 
 def create_features(country_name,
@@ -25,6 +25,7 @@ def create_features(country_name,
                     city_level=False,
                     left_over=False,
                     ua_mode=False,
+                    path_stats='/p/projects/eubucco/stats/5-ft-eng',
                     data_dir='/p/projects/eubucco/data/2-database-city-level'
                     ):
     '''
@@ -33,7 +34,7 @@ def create_features(country_name,
         Returns: None
 
         TODO:
-                * improve the get_stats ouputs locations/names
+                * improve the write_stats outputs locations/names
                 * add hot start option for bld_d and blk_d
     '''
 
@@ -49,8 +50,8 @@ def create_features(country_name,
     city_name = os.path.split(list(paths.values())[0])[-2]
     print(city_name)
 
-    buildings = import_csv_w_wkt_to_gdf(paths['geom'], CRS)
-    buffer_ = import_csv_w_wkt_to_gdf(paths['buffer'], CRS)
+    buildings = import_csv_w_wkt_to_gdf(paths['geom'], CRS_UNI)
+    buffer_ = import_csv_w_wkt_to_gdf(paths['buffer'], CRS_UNI)
     indexes_ = buildings.index
     buildings = buildings.append(buffer_).reset_index(drop=True)
 
@@ -77,7 +78,7 @@ def create_features(country_name,
     buildings = buildings.loc[indexes_]
 
     if int_:
-        intersections = import_csv_w_wkt_to_gdf(paths['intersections'], CRS)
+        intersections = import_csv_w_wkt_to_gdf(paths['intersections'], CRS_UNI)
         fts_int = buildings[['id']]
         fts_int['dist_to_closest_int'] = feature_distance_to_closest_intersection(
             np.array(buildings.geometry), np.array(intersections.geometry), intersections.sindex)
@@ -86,18 +87,16 @@ def create_features(country_name,
         fts_int.to_csv(paths['int_fts'], index=False)
         city_fts = pd.concat((city_fts, feature_city_level_intersections(intersections)), axis=1)
 
-
     if str_:
-        streets = import_csv_w_wkt_to_gdf(paths['streets'], CRS)
+        streets = import_csv_w_wkt_to_gdf(paths['streets'], CRS_UNI)
         fts_str = buildings[['id']]
         fts_str = pd.concat((fts_str, features_closest_street(buildings, streets)), axis=1)
         fts_str = pd.concat((fts_str, features_street_distance_based(buildings, streets)), axis=1)
         fts_str.to_csv(paths['str_fts'], index=False)
         city_fts = pd.concat((city_fts, features_city_level_streets(streets)), axis=1)
 
-
     if sbb_:
-        sbb = import_csv_w_wkt_to_gdf(paths['sbb'], CRS)
+        sbb = import_csv_w_wkt_to_gdf(paths['sbb'], CRS_UNI)
         fts_sbb = buildings[['id']]
         fts_sbb = pd.concat((fts_sbb, features_own_sbb(buildings, sbb)), axis=1)
         fts_sbb = pd.concat((fts_sbb, features_sbb_distance_based(buildings, sbb)), axis=1)
@@ -108,6 +107,7 @@ def create_features(country_name,
         print('Generating city-level features...')
         city_fts.to_csv(paths['city_level_fts'], index=False)
 
-
-    end = time.time() - start
-    print('Time elapsed: {}'.format(end))
+    duration = time.time() - start
+    stats = {'city_idx': city_idx}
+    filename = f'{city_idx}_stat-parts'
+    write_stats(stats, duration, path_stats, filename)
--- eubucco/preproc/streets.py
+++ eubucco/preproc/streets.py
@@ -1,13 +1,14 @@
 import os
 import time
+
 import osmnx as ox
 import momepy
 
 from ufo_map.Preprocessing.preproc_streets import *
-from ufo_map.Utils.helpers import import_csv_w_wkt_to_gdf,save_csv_wkt,get_all_paths,get_stats,arg_parser
-from preproc.db_set_up import fetch_GADM_info_country
+from ufo_map.Utils.helpers import import_csv_w_wkt_to_gdf, save_csv_wkt, get_all_paths, write_stats, arg_parser
 
 CRS_UNI = 'EPSG:3035'
+
 
 def download_osm_streets(country_name,
                          path_stats='/p/projects/eubucco/stats/6-streets'):
@@ -22,81 +23,80 @@ def download_osm_streets(country_name,
 
     print(country_name)
 
-    paths_in = get_all_paths(country_name,'/p/projects/eubucco/data/2-database-city-level-v0_1','boundary')
-    paths_out = get_all_paths(country_name,'/p/projects/eubucco/data/2-database-city-level-v0_1','streets')
+    paths_in = get_all_paths(country_name, '/p/projects/eubucco/data/2-database-city-level-v0_1', 'boundary')
+    paths_out = get_all_paths(country_name, '/p/projects/eubucco/data/2-database-city-level-v0_1', 'streets')
 
     n_streets = 0
 
     start = time.time()
 
-    for path_in,path_out in zip(paths_in,paths_out):
+    for path_in, path_out in zip(paths_in, paths_out):
 
         print(path_out)
 
-        boundary = import_csv_w_wkt_to_gdf(path_in,CRS_UNI,geometry_col='boundary_GADM_2k_buffer').to_crs(4326).geometry.iloc[0]
+        boundary = import_csv_w_wkt_to_gdf(
+            path_in, CRS_UNI, geometry_col='boundary_GADM_2k_buffer').to_crs(4326).geometry.iloc[0]
 
-        city_network = ox.graph_from_polygon(boundary, 
-                                         simplify=True, 
-                                         network_type='drive')
+        city_network = ox.graph_from_polygon(boundary,
+                                             simplify=True,
+                                             network_type='drive')
 
-        city_streets = ox.utils_graph.graph_to_gdfs(city_network, 
-                                       nodes=False, 
-                                       edges=True,
-                                       node_geometry=False, 
-                                       fill_edge_geometry=True).to_crs(CRS_UNI)[['osmid','highway','length','geometry']]
+        city_streets = ox.utils_graph.graph_to_gdfs(city_network,
+                                                    nodes=False,
+                                                    edges=True,
+                                                    node_geometry=False,
+                                                    fill_edge_geometry=True).to_crs(CRS_UNI)[['osmid', 'highway', 'length', 'geometry']]
 
         n_streets += len(city_streets)
 
-        save_csv_wkt(city_streets,path_out)
+        save_csv_wkt(city_streets, path_out)
 
-    end = time.time() - start
+    duration = time.time() - start
+    stats = {'country_name': country_name, 'n_streets': n_streets}
+    filename = f'{country_name}_stat_ddl'
+    write_stats(stats, duration, path_stats, filename)
 
-    get_stats({'country_name':country_name,'n_streets':n_streets},end,path_stats,country_name,'stat_ddl')
 
-
-
-def create_streets_and_intersections(streets,buildings,path_str,path_int):
+def create_streets_and_intersections(streets, buildings, path_str, path_int):
     '''
         Modifies and saves in the relevant folder city-level street file from street linestrings.
 
-        Returns: None 
+        Returns: None
     '''
 
     streets = momepy.gdf_to_nx(streets)
 
     print('Creating intersections...')
     intersections = momepy.nx_to_gdf(streets, lines=False)
-    save_csv_wkt(intersections,path_int)
+    save_csv_wkt(intersections, path_int)
     len_intersections = len(intersections)
     del intersections
 
     print('Adding centrality metrics to streets...')
-    streets = momepy.betweenness_centrality(streets,mode='edges',name='betweenness_metric_e')
-    streets = momepy.closeness_centrality(streets,name='closeness_global')
-    streets = momepy.closeness_centrality(streets,radius=500,name='closeness500',distance='mm_len',weight='mm_len')
-    streets = network_to_street_gdf(streets,buildings)
-    
-    return(streets, len(streets),len_intersections)
+    streets = momepy.betweenness_centrality(streets, mode='edges', name='betweenness_metric_e')
+    streets = momepy.closeness_centrality(streets, name='closeness_global')
+    streets = momepy.closeness_centrality(streets, radius=500, name='closeness500', distance='mm_len', weight='mm_len')
+    streets = network_to_street_gdf(streets, buildings)
+
+    return(streets, len(streets), len_intersections)
 
 
-
-def create_sbb(streets,path_sbb):
+def create_sbb(streets, path_sbb):
     '''
         Creates and saves in the relevant folder city-level street-based blocks file from street linestrings.
 
         Needs to be run on a clean street file created through the ufo-map `rm_duplicates_osm_streets` function,
         here called within `create_streets_and_intersections`.
 
-        Returns: None 
+        Returns: None
     '''
     print('Creating street-based blocks...')
     sbb = generate_sbb(streets)
-    save_csv_wkt(sbb,path_sbb)
+    save_csv_wkt(sbb, path_sbb)
     return(len(sbb))
 
 
-
-def parse_streets(country_name,left_over=False):
+def parse_streets(country_name, left_over=False, path_stats='/p/projects/eubucco/stats/6-streets'):
     '''
         Parses Openstreetmap street network to create an updated <city>_streets.csv with computed network centrality metrics,
         a <city>_intersections.csv file and a <city>_sbb.csv with street-based blocks created by polygonizing the street linestrings.
@@ -106,31 +106,27 @@ def parse_streets(country_name,left_over=False):
         Returns: None
     '''
 
-    args = arg_parser(['i'])
-    print(args.i)
+    city_idx = arg_parser(['i']).i
+    print(city_idx)
 
     start = time.time()
 
     paths = {}
-    for file in ['streets','geom','intersections','sbb']:
-        if left_over==False:
-            paths[file] = get_all_paths(country_name,'/p/projects/eubucco/data/2-database-city-level-v0_1',file)[args.i]
-        else:
-            paths[file] = get_all_paths(country_name,'/p/projects/eubucco/data/2-database-city-level-v0_1',file,left_over)[args.i]
+    for file in ['streets', 'geom', 'intersections', 'sbb']:
+        paths[file] = get_all_paths(
+            country_name, '/p/projects/eubucco/data/2-database-city-level-v0_1', file, left_over)[city_idx]
 
-    path_stats = os.path.join('/p/projects/eubucco/data/2-database-city-level',
-                              country_name,
-                              'stats-parts-str')
- 
-    streets = import_csv_w_wkt_to_gdf(paths['streets'],CRS_UNI)
-    buildings = import_csv_w_wkt_to_gdf(paths['geom'],CRS_UNI)
+    streets = import_csv_w_wkt_to_gdf(paths['streets'], CRS_UNI)
+    buildings = import_csv_w_wkt_to_gdf(paths['geom'], CRS_UNI)
 
-    streets, n_streets, n_int = create_streets_and_intersections(streets,buildings,paths['streets'],paths['intersections']) 
+    streets, n_streets, n_int = create_streets_and_intersections(
+        streets, buildings, paths['streets'], paths['intersections'])
 
-    n_sbb = create_sbb(streets,paths['sbb'])
+    n_sbb = create_sbb(streets, paths['sbb'])
 
-    save_csv_wkt(streets,paths['streets'])
+    save_csv_wkt(streets, paths['streets'])
 
-    end = time.time() - start    
-
-    get_stats({'city_idx':args.i,'n_streets':n_streets,'n_int':n_int,'n_sbb':n_sbb},end,path_stats,str(args.i),'stat_str')
+    duration = time.time() - start
+    stats = {'city_idx': city_idx, 'n_streets': n_streets, 'n_int': n_int, 'n_sbb': n_sbb}
+    filename = f'{city_idx}_stat_str'
+    write_stats(stats, duration, path_stats, filename)
