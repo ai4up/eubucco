e173e03947c1232ad67f04a96ff7a711ed37b4da
Florian Nachtigall <nachtigall.florian@gmail.com>
2022-05-31 16:47:02 +0200

    Improve abstraction of streets downloading & parsing
    From a clean code perspective, similarly to bad7f0d, the
    download_osm_streets() and parse_streets() functions should only deal
    with one thing.  All additional logic which requires the function to
    know many details about how and where it is run, i.e. how to extract the
    correct city form a country's paths_*.txt file, have been extracted to
    the callers of the functions.
    
    download_osm_streets() has been splitted in two: one for downloading an
    individual city and one for downloading all cities within one country.
    
    The imports have been refactored as well to avoid ambiguous "import *".

--- database/preprocessing/6-streets/downloading-streets.py
+++ database/preprocessing/6-streets/downloading-streets.py
@@ -4,6 +4,15 @@ import sys
 PROJECT_SRC_PATH = os.path.realpath(os.path.join(__file__, '..', '..', '..', '..', 'eubucco'))
 sys.path.append(PROJECT_SRC_PATH)
 
-from preproc.streets import download_osm_streets  # noqa: E402
+from ufo_map.Utils.helpers import arg_parser  # noqa: E402
+from preproc.streets import download_osm_streets, download_osm_streets_country  # noqa: E402
 
-download_osm_streets('spain')
+# utilize Slurm's concurrent job scheduling by mapping SLURM_ARRAY_TASK_ID to city indices
+args = arg_parser([('p', str)])
+city_path = args.p
+print(city_path)
+
+if city_path:
+    download_osm_streets(city_path)
+else:
+    download_osm_streets_country()
