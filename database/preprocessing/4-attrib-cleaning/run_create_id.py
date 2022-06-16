ff73a39a8ec2611428078a3a0b24da68a60aae9e
Florian Nachtigall <nachtigall.florian@gmail.com>
2022-06-16 19:14:35 +0200

    Fix incorrect alignment of new id
    The create_id func separately created a new building id for different
    files (e.g. _attrib.csv or _geom.csv) while relying on the ordering of
    buildings in the files to ensure consistency between them.
    
    This resulted in inconsistencies as the ordering differed between the
    files and the mapping of the old id_source and new id column was broken
    across files.
    
    Now, the new id is solely created for the _attrib.csv and other files
    are aligned based on the mapping of id_source and id.
    
    (cherry picked from commit 04204b4d0e4b009518ba9bf171014bac7cec3ce3)

--- database/preprocessing/4-attrib-cleaning/run_create_id.py
+++ database/preprocessing/4-attrib-cleaning/run_create_id.py
@@ -4,10 +4,16 @@ import sys
 PROJECT_SRC_PATH = os.path.realpath(os.path.join(__file__, '..', '..', '..', '..', 'eubucco'))
 sys.path.append(PROJECT_SRC_PATH)
 
+from preproc.parsing import get_params  # noqa: E402
 from preproc.create_unique_ids import create_id  # noqa: E402
+from ufo_map.Utils.helpers import arg_parser  # noqa: E402
 
-create_id(db_version=0.1,
-          path_to_param_file='/p/projects/eubucco/git-eubucco/database/preprocessing/1-parsing/inputs-parsing.csv',
+params_file_path = '/p/projects/eubucco/data/0-raw-data/id_look_up/country-ids'
+args = arg_parser(['i'])
+country = get_params(args.i, params_file_path)['country']
+
+create_id(country,
+          db_version=0.1,
           path_old_db_folder='/p/projects/eubucco/data/2-database-city-level',
           path_new_db_folder='/p/projects/eubucco/data/2-database-city-level-v0_1',
           path_root_id='/p/projects/eubucco/data/0-raw-data/id_look_up/country-ids')
--- eubucco/preproc/create_unique_ids.py
+++ eubucco/preproc/create_unique_ids.py
@@ -5,86 +5,54 @@ import glob
 from ufo_map.Utils.helpers import *
 from preproc.parsing import get_params
 
-# declare global var
 CRS_UNI = 'EPSG:3035'
-
-# def func to assign unqiue ids
 
 
 def assign_unqiue_ids(path, len_df, df_id_mapper, db_version):
-    # get city name
     city = os.path.split(path)[-1]
-    # get id code
-    str_id_code = df_id_mapper.loc[df_id_mapper.city_name == city].id_marker.values[0]
-    # create series with length df
-    return ('v' + str(db_version) + '-' + str_id_code + '-' + pd.Series(range(len_df)).astype('string'))
+    city_id_code = df_id_mapper.loc[df_id_mapper.city_name == city].id_marker.values[0]
+    return ('v' + str(db_version) + '-' + city_id_code + '-' + pd.Series(range(len_df)).astype('string'))
 
 
-def create_id(db_version=0.1,
-              path_to_param_file='/p/projects/eubucco/git-eubucco/database/preprocessing/1-parsing/inputs-parsing.csv',
+def create_id(country,
+              db_version=0.1,
               path_old_db_folder='/p/projects/eubucco/data/2-database-city-level',
               path_new_db_folder='/p/projects/eubucco/data/2-database-city-level-v0_1',
               path_root_id='/p/projects/eubucco/data/0-raw-data/id_look_up/country-ids'):
 
-    #import argparser
-    args = arg_parser(['i'])
-    print(args.i)
-    # import parameters
-    p = get_params(args.i, path_to_param_file)
+    df_id_mapper = pd.read_csv(os.path.join(path_root_id, country + '_ids.csv'))
+    city_paths = get_all_paths(country, path_root_folder=path_old_db_folder)
 
-    print('adding unqiue ids to: ')
-    print(p['country'])
-    print('----------')
+    for i, path in enumerate(city_paths):
+        print('looping through path {} / {}'.format(i, len(city_paths) - 1))
 
-    # read in id mapper
-    df_id_mapper = pd.read_csv(os.path.join(path_root_id, p['country'] + '_ids.csv'))
-    # read in all paths
-    list_city_paths = get_all_paths(p['country'], path_root_folder=path_old_db_folder)
+        # define new, globally unique id for all buildings in _attrib.csv
+        file_path = path + '_attrib.csv'
+        df = pd.read_csv(file_path)
+        df.rename(columns={'id': 'id_source'}, inplace=True)
+        df.sort_values(by='id_source', inplace=True)
+        df['id'] = assign_unqiue_ids(file_path, len(df), df_id_mapper, db_version)
+        id_source_id_mapping = dict(zip(df['id_source'], df['id']))
 
-    i = 0
-    # for path in list_city_paths:
-    for path in list_city_paths:
-        print('looping through path {} / {}'.format(i, len(list_city_paths) - 1))
+        if 'geometry' in df.columns:
+            df = df.drop(columns='geometry')
 
-        # get all parts of folder
-        path_glob = path + '*'
-        path_parts = glob.glob(path_glob)
+        df.to_csv(file_path.replace(path_old_db_folder, path_new_db_folder), index=False)
 
-        for ending in ['_geom', '_attrib', '_attrib_source', '_extra_attrib']:
-            # get path
-            paths_ending = [p for p in path_parts if ending in p]
-            # check for attrib ending
-            if ending == '_attrib':
-                paths_ending = [k for k in paths_ending if 'extra' not in k]
-                paths_ending = [k for k in paths_ending if 'source' not in k]
+        # update other files to use the new id as well
+        for ending in ['_geom', '_buffer', '_attrib_source', '_extra_attrib']:
+            file_path = path + ending + '.csv'
 
-            # only change ids if one file per ending and if not 0
-            if len(paths_ending) == 1:
-                # open file and rename if col
-                df = pd.read_csv(paths_ending[0])
-                df = df.rename(columns={"id": "id_source"})
+            if not os.path.isfile(file_path):
+                print(f'Warning: file {file_path} does not exist')
+                continue
 
-                # assign unqiue ids
-                df['id'] = assign_unqiue_ids(path, len(df), df_id_mapper, db_version)
+            df = pd.read_csv(file_path)
+            df.rename(columns={'id': 'id_source'}, inplace=True)
 
-                # dropping geoms in attrib file
-                if ending == '_attrib':
-                    if 'geometry' in df.columns:
-                        print('dropping geom col for {}'.format(ending))
-                        df = df.drop(columns='geometry')
+            if not 'buffer' in file_path:
+                df['id'] = df['id_source'].map(id_source_id_mapping)
 
-                # save df in new folder structure
-                df.to_csv(paths_ending[0].replace(path_old_db_folder, path_new_db_folder), index=False)
-
-        # rename ids in buffer to id_source
-        paths_ending = [p for p in path_parts if '_buffer' in p]
-        if len(paths_ending) == 1:
-            # open file and rename if col
-            df = pd.read_csv(paths_ending[0])
-            df = df.rename(columns={"id": "id_source"})
-            df.to_csv(paths_ending[0].replace(path_old_db_folder, path_new_db_folder), index=False)
-
-        # increase counter
-        i += 1
+            df.to_csv(file_path.replace(path_old_db_folder, path_new_db_folder), index=False)
 
     print('created unqiue ids. closing run.')
