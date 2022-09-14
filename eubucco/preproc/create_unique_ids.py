d0b1e60a780726839135b3c6b96ace17d2000d8f
wagnerfe <wagner@mcc-berlin.net>
2022-09-14 17:46:37 +0200

    Adds folders when creating unique ids and tests for id errors.
    When creating new ids, folders will be created when they do not exist yet. This is the case
    f.e. when creating a new release version. In addition, we add tests to check that for every city
    we have the same number of entries across all files (geom, attrib, extra_attrib, source_file) and
    no id or id_source duplicates in any of the files.

--- eubucco/preproc/create_unique_ids.py
+++ eubucco/preproc/create_unique_ids.py
@@ -3,10 +3,6 @@ import os
 import glob
 
 from ufo_map.Utils.helpers import *
-from preproc.parsing import get_params
-
-CRS_UNI = 'EPSG:3035'
-
 
 def assign_unqiue_ids(path, len_df, df_id_mapper, db_version):
     city = os.path.split(path)[-1]
@@ -18,11 +14,21 @@ def save_df(df_tmp,path):
         outname = path.rsplit('/',1)[1]
         outdir = path.rsplit('/',1)[0]
         if not os.path.exists(outdir):
-            os.mkdir(outdir)
+            os.makedirs(outdir)
         fullname = os.path.join(outdir, outname)    
     else: fullname=path
 
     df_tmp.to_csv(fullname, index=False)
+
+
+def test_ids(len_df, ids_df,source_ids_df,dupls_source_id_df,dupls_id_df):
+    if not len(set(len_df)) == 1: raise ValueError('Error! Files do not have same length')
+    if any(dupls_id_df): raise ValueError('Error! Dupl ids found')
+    if any(dupls_source_id_df): raise ValueError('Error! Dupl source_ids found')
+    if not all([set(el)==set(ids_df[0]) for el in ids_df]): raise ValueError('Error! Ids do not match across files')
+    if not all([set(el)==set(source_ids_df[0]) for el in source_ids_df]): raise ValueError('Error! Source_ids do not match across files')
+    return True
+
 
 def create_id(country,
               db_version=0.1,
@@ -35,21 +41,26 @@ def create_id(country,
 
     for i, path in enumerate(city_paths):
         print('looping through path {} / {}'.format(i, len(city_paths) - 1))
-
         # define new, globally unique id for all buildings in _attrib.csv
         file_path = path + '_attrib.csv'
         df = pd.read_csv(file_path)
-        df = df.drop_duplicates(subset='id') # drop all left over duplicates
+        df = df.drop_duplicates(subset='id').reset_index(drop=True) # drop all left over duplicates
         df.rename(columns={'id': 'id_source'}, inplace=True)
         df.sort_values(by='id_source', inplace=True)
-        df['id'] = assign_unqiue_ids(file_path, len(df), df_id_mapper, db_version)
+        df['id'] = assign_unqiue_ids(path, len(df), df_id_mapper, db_version)
         id_source_id_mapping = dict(zip(df['id_source'], df['id']))
 
         if 'geometry' in df.columns:
             df = df.drop(columns='geometry')
 
-        #df.to_csv(file_path.replace(path_old_db_folder, path_new_db_folder), index=False)
         save_df(df, file_path.replace(path_old_db_folder, path_new_db_folder))
+
+        # intialize variables for checking that there are no id errors across files
+        len_df = [len(df)]
+        ids_df = [df.id.tolist()]
+        source_ids_df = [df.id_source.tolist()]
+        dupls_id_df = [df.duplicated(subset='id').any()]
+        dupls_source_id_df = [df.duplicated(subset='id_source').any()]
 
         # update other files to use the new id as well
         for ending in ['_geom', '_buffer', '_attrib_source', '_extra_attrib']:
@@ -58,14 +69,23 @@ def create_id(country,
             if not os.path.isfile(file_path):
                 print(f'Warning: file {file_path} does not exist')
                 continue
-
-            df = pd.read_csv(file_path)
+            
+            df = pd.read_csv(file_path) 
+            df = df.drop_duplicates(subset='id').reset_index(drop=True) # drop all left over duplicates
             df.rename(columns={'id': 'id_source'}, inplace=True)
-
+            
             if not 'buffer' in file_path:
                 df['id'] = df['id_source'].map(id_source_id_mapping)
-
-            #df.to_csv(file_path.replace(path_old_db_folder, path_new_db_folder), index=False)
-            save_df(df, file_path.replace(path_old_db_folder, path_new_db_folder))
+            
+            if ending in ['_geom','_attrib_source','_extra_attrib']: 
+                len_df.append(len(df))
+                ids_df.append(df.id.tolist())
+                source_ids_df.append(df.id_source.tolist())
+                dupls_id_df.append(df.duplicated(subset='id').any())
+                dupls_source_id_df.append(df.duplicated(subset='id_source').any())
+                
+                save_df(df, file_path.replace(path_old_db_folder, path_new_db_folder))
+        
+        test_ids(len_df, ids_df, source_ids_df,dupls_source_id_df,dupls_id_df)
 
     print('created unqiue ids. closing run.')
