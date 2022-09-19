be419dbd24446a399591a739041541ae97d288b8
Florian Nachtigall <nachtigall.florian@gmail.com>
2022-09-19 17:04:05 +0200

    Remove unnecessary param defaults for db set-up

--- database/preprocessing/2-db-set-up/db-set-up.py
+++ database/preprocessing/2-db-set-up/db-set-up.py
@@ -3,19 +3,11 @@ import os
 import json
 
 PROJECT_SRC_PATH = os.path.realpath(os.path.join(__file__, '..', '..', '..', '..', 'eubucco'))
-
 sys.path.append(PROJECT_SRC_PATH)
 
-from preproc.db_set_up import db_set_up  # noqa: E402
+import preproc # noqa: E402
 
 # function parameters are passed by slurm-pipeline via stdin
 params = json.load(sys.stdin)
 print(params)
-
-db_set_up(
-    **params,
-    chunksize=int(5E5),
-    folders=True,
-    boundaries=True,
-    bldgs=True,
-    auto_merge=True)
+preproc.db_set_up(**params)
