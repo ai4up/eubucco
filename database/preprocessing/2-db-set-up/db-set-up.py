60f47ce650797cdf64ec09c0afe4de4b631cc85d
Florian Nachtigall <nachtigall.florian@gmail.com>
2022-10-10 12:02:07 +0200

    Fix incorrect import path
    Also, remove deprecated & outdated slurm submit.sh file.

--- database/preprocessing/2-db-set-up/db-set-up.py
+++ database/preprocessing/2-db-set-up/db-set-up.py
@@ -5,9 +5,9 @@ import json
 PROJECT_SRC_PATH = os.path.realpath(os.path.join(__file__, '..', '..', '..', '..', 'eubucco'))
 sys.path.append(PROJECT_SRC_PATH)
 
-import preproc # noqa: E402
+from preproc import db_set_up # noqa: E402
 
 # function parameters are passed by slurm-pipeline via stdin
 params = json.load(sys.stdin)
 print(params)
-preproc.db_set_up(**params)
+db_set_up.db_set_up(**params)
