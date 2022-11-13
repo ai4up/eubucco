0ffa8d490d04f4b030e41d57d1be0a67099688e2
Florian Nachtigall <nachtigall.florian@gmail.com>
2022-11-13 20:29:25 +0100

    Fix params for OSM street downloading

--- database/preprocessing/6-streets/downloading-streets.py
+++ database/preprocessing/6-streets/downloading-streets.py
@@ -5,13 +5,10 @@ import json
 PROJECT_SRC_PATH = os.path.realpath(os.path.join(__file__, '..', '..', '..', '..', 'eubucco'))
 sys.path.append(PROJECT_SRC_PATH)
 
-from preproc.streets import download_osm_streets, download_osm_streets_country  # noqa: E402
+from preproc.streets import download_osm_streets_country  # noqa: E402
 
 # function parameters are passed by slurm-pipeline via stdin
 params = json.load(sys.stdin)
 print(params)
 
-if params:
-    download_osm_streets(**params)
-else:
-    download_osm_streets_country()
+download_osm_streets_country(**params)
