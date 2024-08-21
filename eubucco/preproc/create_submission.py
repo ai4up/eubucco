90d24dcbfb15d23a6c4d583c5ebafec9f4d2087c
Nikola Milojevic-Dupont <milojevic@mcc-berlin.net>
2024-08-21 18:27:33 +0200

    Update create_submission.py

--- eubucco/preproc/create_submission.py
+++ eubucco/preproc/create_submission.py
@@ -77,7 +77,10 @@ def merge_city_files(path):
 
         # merge on id
         df_temp = df_geom.merge(df_attrib, on='id')
-        df_temp = df_temp[['id', 'height', 'age', 'type', 'id_source', 'type_source', 'geometry']]
+        if 'microsoft-circeular' not in path:
+            df_temp = df_temp[['id', 'height', 'age', 'type', 'id_source', 'type_source', 'geometry']]
+        else:
+            df_temp = df_temp[['id', 'height', 'age', 'type', 'type_source', 'geometry']]        
     else:
         df_temp = pd.DataFrame()
         print('!!! empty city!!!')
