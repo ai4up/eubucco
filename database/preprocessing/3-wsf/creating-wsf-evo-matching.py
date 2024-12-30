4489d6d72db0a82bf74667c1a6ca23920f6ef5a3
Nikola Milojevic-Dupont <milojevic@mcc-berlin.net>
2024-12-30 15:30:27 +0100

    Remove old code for WSF

--- database/preprocessing/3-wsf/creating-wsf-evo-matching.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-PROJECT_SRC_PATH = os.path.realpath(os.path.join(__file__, '..', '..', '..', '..', 'eubucco'))
-sys.path.append(PROJECT_SRC_PATH)
-
-from preproc.wsf import create_wsf_evo_matching  # noqa: E402
-
-create_wsf_evo_matching('NLD')
