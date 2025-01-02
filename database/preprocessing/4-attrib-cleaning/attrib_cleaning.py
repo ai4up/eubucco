236fe486eedda3e41388295788df2e586c4cf02f
Florian Nachtigall <nachtigall.florian@gmail.com>
2025-01-02 19:23:08 +0100

    Refactor 4-attrib-cleaning to support slurm pipeline

---
+++ database/preprocessing/4-attrib-cleaning/attrib_cleaning.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import json
+
+PROJECT_SRC_PATH = os.path.realpath(os.path.join(__file__, '..', '..', '..', '..', 'eubucco'))
+sys.path.append(PROJECT_SRC_PATH)
+
+from preproc.attribs import attrib_cleaning
+
+# function parameters are passed by slurm-pipeline via stdin
+params = json.load(sys.stdin)
+print(params)
+attrib_cleaning(**params)
--- eubucco/preproc/attribs.py
+++ eubucco/preproc/attribs.py
@@ -1,52 +1,102 @@
-import os
+import logging
+from pathlib import Path
+import re
+from typing import Dict, List
+
 import pandas as pd
-import socket
-print(socket.gethostname())
+import geopandas as gpd
+import numpy as np
+from pandas.api.types import CategoricalDtype
+
+FLOOR_HEIGHT = 3  # meter
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.StreamHandler()]
+)
+logger = logging.getLogger(__name__)
 
 
-def get_mapping_dict(dataset_name,
-                     path_type_matches_folder='/p/projects/eubucco/data/0-raw-data/type-matches'
-                     ) -> dict:
+def attrib_cleaning(data_dir: str, out_dir: str, type_mapping_path: str, db_version: str, file_pattern: str = None) -> None:
+    for f in _all_files(data_dir, file_pattern):
+        try:
+            logger.info(f'Cleaning attributes for {f.name}...')
+            df = gpd.read_parquet(f)
+
+            df = unique_ids(df, db_version)
+            df = type_mapping(df, type_mapping_path)
+            df = age_cleaning(df)
+            df = height_cleaning(df)
+
+            out_dir = Path(out_dir).mkdir(parents=True, exist_ok=True)
+            out_path = out_dir / f.name
+            df.to_parquet(out_path)
+
+        except Exception:
+            logger.exception(
+                f'Exception occurred while cleaning attributes for file {f.name}. Skipping {f.name} and continuing...')
+
+
+def height_cleaning(df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    df = _estimate_height_from_floors(df)
+    df['height'] = df['height'].astype(float)
+
+    return df
+
+
+def age_cleaning(df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    df_fr_es = df[df['LAU_ID'].str[:2].isin(['FR', 'ES'])]
+    df['age'] = df_fr_es['age'].dropna().str[:4]  # extract year from YYYY-MM-DD encoded string
+    df['age'] = df['age'].astype(float)
+
+    return df
+
+
+def type_mapping(df: gpd.GeoDataFrame, type_mapping_path: str) -> gpd.GeoDataFrame:
+    bldg_types = pd.read_csv(type_mapping_path)
+    type_mapping = bldg_types.set_index("type_source")["type"].to_dict()
+    res_type_mapping = bldg_types.set_index("type_source")["residential_type"].to_dict()
+
+    df["type"] = _harmonize_type(df["type_source"], type_mapping)
+    df["residential_type"] = _harmonize_type(df["type_source"], res_type_mapping)
+
+    return df
+
+
+def unique_ids(df, db_version):
+    df['id_source'] = df['id']
+    df['id'] = 'v' + str(db_version) + '-' + df['LAU_ID'] + '-' + pd.Series(range(len(df)), dtype=str)
+
+    return df
+
+
+def _all_files(data_dir: str, pattern: str = None) -> List[Path]:
+    paths = Path(data_dir).rglob("*")
+    files = [p for p in paths if p.is_file()]
+
+    if pattern:
+        pattern = re.compile(pattern)
+        files = [f for f in files if pattern.match(f.name)]
+
+    return files
+
+
+def _estimate_height_from_floors(df):
+    df['height_source'] = df['height']
+    df['height_source'] = df['height_source'].fillna('floors')
+    df['height'] = df['height'].fillna(df['floors'] * FLOOR_HEIGHT)
+
+    return df
+
+
+def _harmonize_type(source_type: pd.Series, type_mapping: Dict[str, str]) -> pd.Series:
     '''
-        Creates dict with {type_source:type_db} key/value pairs
-        from a csv with such information available as columns.
+        Maps buildings types from the source dataset to harmonized types for each building in a city.
     '''
 
-    if dataset_name in ['hamburg-gov', 'brandenburg-gov', 'sachsen-gov', 'sachsen-anhalt-gov',
-                        'thuerigen-gov', 'nordrhein-westfalen-gov', 'niedersachsen-gov']:
-        dataset_name = 'germany-alkis'
+    types = set(type_mapping.values())
+    types.remove(np.nan)
+    harm_type = source_type.map(type_mapping).astype(CategoricalDtype(categories=types))
 
-    elif dataset_name in ['calabria-gov', 'lazio-gov', 'piemonte-gov', 'veneto-gov']:
-        dataset_name = 'italy-edifc-uso'
-
-    elif 'osm' in dataset_name:
-        dataset_name = 'osm'
-
-    df_types = pd.read_csv(os.path.join(path_type_matches_folder, dataset_name + '_type_matches.csv'))
-    df_types['type_db'] = [x if isinstance(x, str) else '' for x in df_types.type_db]
-
-    return {x: y for x, y in zip(df_types.type_source, df_types.type_db)}
-
-
-def type_mapping(df_bldgs, mapping) -> pd.DataFrame:
-    '''
-        Maps buildings types from the source dataset to residential, non-residential
-        or unknown for each building in a city.
-
-        Parameters:
-        * mapping: dict with {type_source:type_db} key/value pairs
-        * df_types: pd.DataFrame with the columns 'code' and 'type_db'
-
-        Returns: pd.DataFrame with new types in the 'type' column
-    '''
-
-    print(f'Could not match {len([x for x in df_bldgs.type_source if x not in mapping.keys()])} source types')
-    print(f'Could not match: {set([x for x in df_bldgs.type_source if x not in mapping.keys()])}')
-
-    df_bldgs['type'] = [mapping[x] if x in mapping.keys() else '' for x in df_bldgs.type_source]
-
-    return df_bldgs
-
-
-def add_floor_as_height(df,floor_height):
-    return [x if str(x) != 'nan' else y * floor_height for x, y in zip(df.height, df.floors)]
+    return harm_type
