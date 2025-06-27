64cbc2a8834e3f2f1abc9213c0497a4288b20b29
wagnerfe <wagner@mcc-berlin.net>
2025-06-27 19:52:38 +0200

    Add first test to run Bremen and Hamburg parsing on Cluster

--- database/preprocessing/1-parsing/main-parsing.py
+++ database/preprocessing/1-parsing/main-parsing.py
@@ -6,6 +6,9 @@ sys.path.append(PROJECT_SRC_PATH)
 
 from preproc.parsing import parse  # noqa: E402
 
-# parse(test_run=False)
-
-parse(path_output='/p/projects/eubucco/data/1-intermediary-outputs-v1-osm',test_run=False)
+parse(path_to_param_file='/p/projects/eubucco/git-eubucco/database/preprocessing/1-parsing/inputs-parsing-v1-cluster-test.csv',
+          path_output='/p/projects/eubucco/data/1-intermediary-outputs-v1-gov2',
+          path_stats='/p/projects/eubucco/stats/1-parsing-v1/',
+          test_run=True,
+          arg_as_param=True
+          )
