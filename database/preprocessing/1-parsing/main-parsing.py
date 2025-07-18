ca02a940003016426d10a28afa8900598e789685
wagnerfe <wagner@mcc-berlin.net>
2025-07-18 15:26:05 +0200

    Minor fixes to last commit to run all regions of Germany

--- database/preprocessing/1-parsing/main-parsing.py
+++ database/preprocessing/1-parsing/main-parsing.py
@@ -6,10 +6,9 @@ sys.path.append(PROJECT_SRC_PATH)
 
 from preproc.parsing import parse  # noqa: E402
 
-parse(path_to_param_file='/p/projects/eubucco/git-eubucco/database/preprocessing/1-parsing/inputs-parsing-v1-cluster-test.csv',
+parse(path_to_param_file='/p/projects/eubucco/git-eubucco/database/preprocessing/1-parsing/inputs-parsing-v1.csv',
           path_output='/p/projects/eubucco/data/1-intermediary-outputs-v1-gov2',
           path_stats='/p/projects/eubucco/stats/1-parsing-v1/',
-          test_run=True,
-          arg_as_param=True
+          test_run=True
           )
 
